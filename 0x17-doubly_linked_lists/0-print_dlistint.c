c86d22aab839929a8bf0d0b0560202f46970fe51
breign3 <brainy.ngwenya@gmail.com>
2024-03-23 12:30:49 -0700

    0-print_dlistint.c

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -9,19 +9,16 @@
 
 size_t print_dlistint(const dlistint_t *h)
 {
-const dlistint_t *ptr = h;
-size_t n = 0;
+	const dlistint_t *ptr = h;
+	size_t n = 0;
 
-	for (; ptr && ptr->prev; ptr = ptr->prev)
+	while (ptr && ptr->prev)
+		ptr = ptr->prev;
+	while (ptr)
 	{
-
+		printf("%d\n", ptr->n);
+		n++;
+		ptr = ptr->next;
 	}
-
-	for (; ptr; ptr = ptr->next)
-		{
-			printf("%d\n", ptr->n);
-			n++;
-		}
-
 	return (n);
 }

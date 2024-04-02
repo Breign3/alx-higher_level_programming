e7514dc872321b52c210511b8b16c80789abac65
breign3 <brainy.ngwenya@gmail.com>
2024-04-02 13:59:49 -0700

    2-safe_print_list_integers.py

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-def safe_print_integer(value):
 
-"""Print an integer with "{:d}".format().
+def safe_print_integer(value):
+    """Print an integer with "{:d}".format().
 
     Args:
         value (int): The integer to print.
@@ -9,7 +9,7 @@ def safe_print_integer(value):
     Returns:
         If a TypeError or ValueError occurs - False.
         Otherwise - True.
-"""
+    """
     try:
         print("{:d}".format(value))
         return (True)

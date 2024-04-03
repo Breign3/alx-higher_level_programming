a685dd4f3fa4106e75d7d0aa4c9cf8fe56e737cf
breign3 <brainy.ngwenya@gmail.com>
2024-04-02 19:08:19 -0700

    3-safe_print_division.py

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 
-    """
-    Divides 2 integers and prints the result.
+def safe_print_division(a, b):
+
+    """Print an integer with "{:d}".format()
 
             Args:
             a: Integer
             b: Integer
-            Returns: The value of the division, otherwise: None
-    """
 
-def safe_print_division(a, b):
+        Returns: The value of the division, otherwise: None
+    """
     quotient = None
     try:
         quotient = a / b

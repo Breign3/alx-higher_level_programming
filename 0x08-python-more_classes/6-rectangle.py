b5499164ebab3466dd48a7f92fd5c69dd57136dc
breign3 <brainy.ngwenya@gmail.com>
2024-04-08 12:35:14 -0700

    6-rectangle.py

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -51,14 +51,14 @@ class Rectangle:
 
     def perimeter(self):
         """ returns rectangle perimiter"""
-        if self.__width is 0 or self.__height is 0:
+        if self.__width == 0 or self.__height == 0:
             return 0
         return self.__width * 2 + self.__height * 2
 
     def __str__(self):
         """ return the rectangle with the character #
         """
-        if self.__width is 0 or self.__height is 0:
+        if self.__width == 0 or self.__height == 0:
             return ""
         return ("\n".join(["".join(["#" for i in range(self.__width)])
                 for j in range(self.__height)]))

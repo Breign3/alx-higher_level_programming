68c7e7db58b4dfe418d0326c126d21a59de05481
Breign3 <brainy.ngwenya@gmail.com>
2024-05-23 12:21:55 -0700

    12-no_cheating.sql

--- 0x0D-SQL_introduction/102-rating_shows.sql
+++
@@ -1,8 +0,0 @@
--- lists all shows from hbtn_0d_tvshows_rate by their rating
--- lists all rows of a table by the sum of a linked row
-SELECT `title`, SUM(`rate`) AS `rating`
-  FROM `tv_shows` AS t
-       INNER JOIN `tv_show_ratings` AS r
-       ON t.`id` = r.`show_id`
- GROUP BY `title`
- ORDER BY `rating` DESC;

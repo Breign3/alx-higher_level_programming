2e2a52b83324b967862caca0013bccaf37138af9
Breign3 <brainy.ngwenya@gmail.com>
2024-05-23 11:54:30 -0700

    100-not_my_genres.sql

--- 0x0D-SQL_introduction/100-not_my_genres.sql
+++ 0x0D-SQL_introduction/100-not_my_genres.sql
@@ -1,12 +1,19 @@
 -- uses the hbtn_0d_tvshows database to list all genres not linked to the show Dexter
 -- uses a database to list all rows not linked to one row
-SELECT g.name
-FROM tv_genres g
-WHERE g.name NOT IN (
-      SELECT g.name
-      FROM tv_genres g
-      INNER JOIN tv_show_genres m ON g.id = m.genre_id
-      INNER JOIN tv_shows s ON m.show_id = s.id
-      WHERE s.title = 'Dexter'
-)
-ORDER BY g.name ASC;
+SELECT DISTINCT `name`
+  FROM `tv_genres` AS g
+       INNER JOIN `tv_show_genres` AS s
+       ON g.`id` = s.`genre_id`
+
+       INNER JOIN `tv_shows` AS t
+       ON s.`show_id` = t.`id`
+       WHERE g.`name` NOT IN
+             (SELECT `name`
+                FROM `tv_genres` AS g
+	             INNER JOIN `tv_show_genres` AS s
+		     ON g.`id` = s.`genre_id`
+
+		     INNER JOIN `tv_shows` AS t
+		     ON s.`show_id` = t.`id`
+		     WHERE t.`title` = "Dexter")
+ ORDER BY g.`name`;

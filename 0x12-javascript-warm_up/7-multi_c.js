6978fca9ccc6de29e616253440b530f42ca5424b
Breign3 <brainy.ngwenya@gmail.com>
2024-06-18 08:34:54 -0700

    7-multi_c.js

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,16 +1,9 @@
 #!/usr/bin/node
-
-const argument = process.argv[2];  
-const num = parseInt(argument, 10);
-
-if (isNaN(num)) {
-  console.log('Missing number of occurrences'); 
+const x = Math.floor(Number(process.argv[2]));
+if (isNaN(x)) {
+  console.log('Missing number of occurrences');
 } else {
-  (function printMessage(n, message) {  
-    if (n > 0) { 
-      console.log(message);
-      printMessage(n - 1, message); 
-    }
-  })(num, 'C is fun');
+  for (let i = 0; i < x; i++) {
+    console.log('C is fun');
+  }
 }
-

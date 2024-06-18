cb3238421bfddf6fa94180530fb1f0fda4e553e2
Breign3 <brainy.ngwenya@gmail.com>
2024-06-18 08:48:16 -0700

    102-object_fct.js

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -4,13 +4,12 @@ const myObject = {
   value: 12
 };
 console.log(myObject);
-/*
-YOUR CODE HERE
-*/
+myObject.incr = function () {
+  myObject.value += 1;
+};
 myObject.incr();
 console.log(myObject);
 myObject.incr();
 console.log(myObject);
 myObject.incr();
 console.log(myObject);
-

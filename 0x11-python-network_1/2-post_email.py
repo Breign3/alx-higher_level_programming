5c981d35270acfcdf89e36c436da74d18f0ff178
breign3 <brainy.ngwenya@gmail.com>
2024-07-05 16:26:28 -0700

    10-my_github.py

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,14 +1,25 @@
 #!/usr/bin/python3
 
+#!/usr/bin/python3
+"""
+A script that:
+- Takes in a URL and an email,
+- Sends a POST request to the URL with the email as a parameter,
+- Displays the body of the response (decoded in utf-8).
+"""
+
 import sys
-import urllib.parse
 import urllib.request
+import urllib.parse
 
 if __name__ == "__main__":
+    # Get the URL and email from command-line arguments
     url = sys.argv[1]
-    value = {"email": sys.argv[2]}
-    data = urllib.parse.urlencode(value).encode("ascii")
+    email = sys.argv[2]
+    email_bytes = email.encode("utf-8")
+    request = urllib.request.Request(url, data=email_bytes, method="POST")
 
-    request = urllib.request.Request(url, data)
     with urllib.request.urlopen(request) as response:
-        print(response.read().decode("utf-8"))
+        response_body = response.read().decode("utf-8")
+        print(response_body)
+

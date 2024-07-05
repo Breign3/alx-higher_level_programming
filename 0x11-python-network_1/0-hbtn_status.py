f14f76d3ef3a307982c8b5d6c31f328b77e4cd41
breign3 <brainy.ngwenya@gmail.com>
2024-07-05 04:29:30 -0700

    0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
-"""Fetches https://intranet.hbtn.io/status."""
-import urllib.request
+from urllib.request import Request, urlopen
 
 
 if __name__ == "__main__":
-    request = urllib.request.Request("https://intranet.hbtn.io/status")
-    with urllib.request.urlopen(request) as response:
-        body = response.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(body)))
-        print("\t- content: {}".format(body))
-        print("\t- utf8 content: {}".format(body.decode("utf-8")))
+    url = 'https://alx-intranet.hbtn.io/status'
+    request = Request(url)
+
+    with urlopen(request) as response:
+        raw_content = response.read()
+        decoded_content = raw_content.decode('utf-8')
+
+        print('Body response:')
+        print('\t- type: {_type}'.format(_type=type(raw_content)))
+        print('\t- content: {_content}'.format(_content=raw_content))
+        print('\t- utf8 content: {_utf8_c}'.format(_utf8_c=decoded_content))

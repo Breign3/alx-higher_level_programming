bb5ef5e43d3b1e1c1b1892df3349d2c07daa62f0
Breign3 <brainy.ngwenya@gmail.com>
2024-07-31 06:19:58 -0700

    101-starwars_character.js

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -2,24 +2,26 @@
 
 const request = require('request');
 const id = process.argv[2];
+
+if (!id) {
+  console.error('Please provide a film ID as an argument.');
+  process.exit(1);
+}
+
 const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
 
 request.get(url, (error, response, body) => {
   if (error) {
-    console.log(error);
+    console.error('Error fetching film data:', error);
+  } else if (response.statusCode !== 200) {
+    console.error(`Failed to fetch film data. Status code: ${response.statusCode}`);
   } else {
-    const content = JSON.parse(body);
-    const characters = content.characters;
-    // console.log(characters);
-    for (const character of characters) {
-      request.get(character, (error, response, body) => {
-        if (error) {
-          console.log(error);
-        } else {
-          const names = JSON.parse(body);
-          console.log(names.name);
-        }
-      });
-    }
-  }
-});
+    try {
+      const content = JSON.parse(body);
+      const characters = content.characters;
+
+      for (const character of characters) {
+        request.get(character, (charError, charResponse, charBody) => {
+          if (charError) {
+            console.error
+

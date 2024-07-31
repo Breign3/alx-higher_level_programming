6e84b32e9663f8d30c03c2f61040c68f3ef24040
Breign3 <brainy.ngwenya@gmail.com>
2024-07-31 06:48:45 -0700

    heyyy

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,27 +1,29 @@
 #!/usr/bin/node
 
 const request = require('request');
-const id = process.argv[2];
+const endPoint = 'http://swapi-api.hbtn.io/api/films/' + process.argv[2];
 
-if (!id) {
-  console.error('Please provide a film ID as an argument.');
-  process.exit(1);
-}
 
-const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
-
-request.get(url, (error, response, body) => {
-  if (error) {
-    console.error('Error fetching film data:', error);
-  } else if (response.statusCode !== 200) {
-    console.error(`Failed to fetch film data. Status code: ${response.statusCode}`);
-  } else {
-    try {
-      const content = JSON.parse(body);
-      const characters = content.characters;
-
-      for (const character of characters) {
-        request.get(character, (charError, charResponse, charBody) => {
-          if (charError) {
-            console.error
+request.get(endPoint, function (err, response, body) {
+  if (err) {
+    throw err;
+  } else if (response.statusCode === 200) {
+    const characters = JSON.parse(body).characters;
+    const l = [];
+    characters.forEach(character => {
+      l.push(new Promise((resolve, reject) => {
+        request.get(character, function (err, response, body) {
+          if (err) {
+            reject(err);
+          } else if (response.statusCode === 200) {
+            resolve(JSON.parse(body).name);
+          }
+        });
+      }));
+    });
+    Promise.all(l).then(names => {
+      names.forEach(name => console.log(name));
+    });
+  }
+});
 

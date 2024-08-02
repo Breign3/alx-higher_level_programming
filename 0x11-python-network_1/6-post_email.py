ef55da0e1a28974078f366feb90fa910bb279796
Breign3 <brainy.ngwenya@gmail.com>
2024-08-02 15:16:04 -0700

    84526

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
+"""Sends a POST request to a given URL with a given email.
+  - Displays the body of the response.
+"""
+import sys
+import requests
 
-if __name__ == '__main__':
-    from sys import argv
-    from requests import post
 
-    url = argv[1]
-    email = argv[2]
-    res = post(url, {'email': email})
-    print(res.text)
+if __name__ == "__main__":
+    url = sys.argv[1]
+    value = {"email": sys.argv[2]}
+
+    r = requests.post(url, data=value)
+    print(r.text)

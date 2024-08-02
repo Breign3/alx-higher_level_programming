bfb33af9490528f5a526f0091dc710cca9fb5f9c
Breign3 <brainy.ngwenya@gmail.com>
2024-08-02 15:12:22 -0700

    9656

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,19 +1,16 @@
 #!/usr/bin/python3
-"""
-Sends a POST request to the passed URL with the email as a parameter,
-and displays the body of the response (decoded in utf-8)
-"""
+"""Sends a request to the URL and displays the body of the response."""
 
-if __name__ == '__main__':
-    import urllib.request
-    import urllib.parse
-    import sys
+import sys
+import urllib.parse
+import urllib.request
 
-    argv = sys.argv
-    url = argv[1]
-    email = argv[2]
-    DATA = urllib.parse.urlencode({"email": email})
-    DATA = DATA.encode('ascii')
 
-    with urllib.request.urlopen(url, DATA) as response:
-        print(response.read().decode('utf-8'))
+if __name__ == "__main__":
+    url = sys.argv[1]
+    value = {"email": sys.argv[2]}
+    data = urllib.parse.urlencode(value).encode("ascii")
+
+    request = urllib.request.Request(url, data)
+    with urllib.request.urlopen(request) as response:
+        print(response.read().decode("utf-8"))

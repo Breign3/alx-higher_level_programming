4853e946fa895d12bc0905f6689b182928b26789
Breign3 <brainy.ngwenya@gmail.com>
2024-08-02 15:18:50 -0700

    98654

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
+"""Sends a request to a given URL and displays the response body.
+  - Handles HTTP errors.
+"""
+import sys
+import requests
 
 
-if __name__ == '__main__':
-    from sys import argv
-    from requests import get
+if __name__ == "__main__":
+    url = sys.argv[1]
 
-    url = argv[1]
-
-    response = get(url)
-    ERR_TXT = 'Error code: {}'
-    status = response.status_code
-    print(ERR_TXT.format(status) if (status >= 400) else response.text)
+    r = requests.get(url)
+    if r.status_code >= 400:
+        print("Error code: {}".format(r.status_code))
+    else:
+        print(r.text)
